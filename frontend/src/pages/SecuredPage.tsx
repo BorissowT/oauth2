4dea1c6e607e3d323ccd7b3ae4de80bb38792f46
IlliaChaban <ic@home-klick.de>
2023-09-06 11:51:10 +0200

    added crud operations for apartments

--- frontend/src/pages/SecuredPage.tsx
+++ frontend/src/pages/SecuredPage.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import {useLazyExampleQuery} from "../store/api/testApiSlice";
-import {useAppSelector} from "../store/store";
+import {
+    useChangeApartmentMutation,
+    useCreateApartmentMutation, useDeleteApartmentMutation,
+    useLazyGetApartmentsQuery
+} from "../store/api/apartmentsSlice";
 
 const SecuredPage = () => {
-    const [trigger, {data}] = useLazyExampleQuery()
-    const token = useAppSelector(state => state.user.token)
-    const sendRequest = () => {
-        trigger()
+
+    const [createApartment, {data: createData, error: createError}] = useCreateApartmentMutation()
+    const [deleteApartment, {data: deleteData, error: deleteError}] = useDeleteApartmentMutation()
+    const [changeApartment, {data: changeData, error: changeError}] = useChangeApartmentMutation()
+    const [fetchApartments, {data: apartmentsData, error: apartmentsError}] = useLazyGetApartmentsQuery()
+
+    const handleCreateApartment = () => {
+        createApartment({name: "New Apartment"})
     }
 
+    const handleDeleteApartment = () => {
+        deleteApartment(1)
+    }
+
+    const handleChangeApartment = () => {
+        changeApartment({id: 1, name: "Changed Apartment"})
+    }
+
+    const handleGetApartments = () => {
+        fetchApartments('')
+    }
+
+    console.log(createError)
+
     return (
-        <div>
-            {
-                data ?
-                    // @ts-ignore
-                    <p>{data.message}</p>
-                    : <></>}
-            <h1 className="text-black text-4xl">Welcome to the Protected Page.</h1>
-            <button onClick={sendRequest}>Send request</button>
-        </div>
+        <>
+            <h1>Welcome to the Secured Page</h1>
+            {/*@ts-ignore*/}
+            <p>Get: {apartmentsError ? apartmentsError.data.error_description : apartmentsData?.message}</p>
+            {/*@ts-ignore*/}
+            <p>Create: {createError ? createError.data.error_description : createData?.message}</p>
+            {/*@ts-ignore*/}
+            <p>Delete: {deleteError ? deleteError.data.error_description : deleteData?.message}</p>
+            {/*@ts-ignore*/}
+            <p>Update: {changeError ? changeError.data.error_description : changeData?.message}</p>
+            <div>
+                <br/>
+                <button onClick={handleCreateApartment}>Create Apartment</button>
+                <button onClick={handleDeleteApartment}>Delete Apartment</button>
+                <button onClick={handleChangeApartment}>Update Apartment</button>
+                <button onClick={handleGetApartments}>Get Apartments</button>
+            </div>
+            <br/>
+            <div></div>
+        </>
     );
 };
 
